f05b1af35c282182e933cbb5b422ac3c8ab9fdb5
Manu Sharma <manusharma@qainfotech.com>
2020-12-18 23:08:23 +0530

    Updated on 18-12-2020

--- projects/entry/entry/urls.py
+++ projects/entry/entry/urls.py
@@ -1,18 +1,4 @@
-"""entry URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""entry URL Configuration"""
 from django.contrib import admin
 from django.urls import path,include
 from . import views
@@ -23,5 +9,6 @@ admin.site.index_title="Entry & Exit System"
 urlpatterns = [
     path('', views.index, name="index"),
     path('director/', include('director.urls')),
+    path('gaurd/', include('gaurd.urls')),
     path('admin/', admin.site.urls),
 ]
--- projects/entry/gaurd/views.py
+++ projects/entry/gaurd/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import gaurds
 
 # Create your views here.
+def gaurd(request):
+    if request.session.get('guser'):
+        return redirect('ghome')
+    else:
+        return render(request,'gaurd.html')
+    
+def glogin(request):
+    guser=request.POST['guser']
+    gpass=request.POST['gpass']
+    res=gaurds.objects.filter(guser=guser, gpass=gpass)
+    if len(res)==1:
+        request.session['guser']=res[0].guser
+        return redirect('ghome')
+    else:
+        return render(request,'ghome.html',{'error':'Username or Password Incorrect!!!'})
+        
+def ghome(request):
+    if request.session.get('guser'):
+        return render(request,'ghome.html')
+    else:
+        return redirect('/gaurd/')
